d0455de3dbc8a13e309c2e7d3351277e40fe0e9f
montival_junior@yahoo.com.br <Europa2017!>
2020-09-23 20:01:09 -0300

    criacao do projeto

---
+++ src/test/java/io/github/monthalcantara/restspringbootestudo/RestSpringBootEstudoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.restspringbootestudo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestSpringBootEstudoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
