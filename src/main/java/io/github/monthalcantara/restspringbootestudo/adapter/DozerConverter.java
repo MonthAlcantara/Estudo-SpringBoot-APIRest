91942529d978f6ff8540d2c3a6baf5a39aeee049
montival Junior <montival_junior@yahoo.com.br>
2020-09-25 16:12:18 -0300

    Adicionada classe de conversão de objetos para VO

---
+++ src/main/java/io/github/monthalcantara/restspringbootestudo/adapter/DozerConverter.java
@@ -0,0 +1,24 @@
+package io.github.monthalcantara.restspringbootestudo.adapter;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DozerConverter {
+
+    private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+
+    public static <Origin, Destination> Destination parseObject(Origin origin, Class<Destination> destination){
+        return mapper.map(origin, destination);
+    }
+
+    public static <Origin, Destination> List<Destination> parseListObjects(List<Origin> origin, Class<Destination> destination){
+        List<Destination> destinationObjects = new ArrayList<>();
+        for(Object o : origin){
+            destinationObjects.add(mapper.map(origin,destination));
+        }
+        return destinationObjects;
+    }
+}
