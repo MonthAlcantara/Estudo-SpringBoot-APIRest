82c17ed7375185cc1b195313a87c5b5ca6466e1f
montival Junior <montival_junior@yahoo.com.br>
2020-09-25 09:50:40 -0300

    Adicionada personalização ao tratamento de exception

--- src/main/java/io/github/monthalcantara/restspringbootestudo/service/PersonService.java
+++ src/main/java/io/github/monthalcantara/restspringbootestudo/service/PersonService.java
@@ -2,10 +2,13 @@ package io.github.monthalcantara.restspringbootestudo.service;
 
 import io.github.monthalcantara.restspringbootestudo.model.Person;
 
+import java.util.List;
+
 public interface PersonService {
-    public Person findById(String id);
+    public Person findById(Long id);
     public Person create(Person person);
     public Person update(Person person);
+    public List<Person> findAll() throws Exception;
     public void delete(Long id);
 
 }
