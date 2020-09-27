a268814be71f8224a07bd4ffb562304519ce1294
montival Junior <montival_junior@yahoo.com.br>
2020-09-27 11:21:54 -0300

    Habilitado suporte ao tipo yml

--- src/main/java/io/github/monthalcantara/restspringbootestudo/config/WebConfig.java
+++ src/main/java/io/github/monthalcantara/restspringbootestudo/config/WebConfig.java
@@ -1,14 +1,26 @@
 package io.github.monthalcantara.restspringbootestudo.config;
 
+import io.github.monthalcantara.restspringbootestudo.serialization.converter.YamlJackson2HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
+    //Criei um Media Type com valor yml para habilitar no método configureContentNegotiation
+    private static final MediaType MEDIA_TYPE_YML = MediaType.valueOf("application/x-yaml");
+
+    /* Adicionando o tipo Yml a lista de conversores de mensagens Http com a classe criada em serialization>>converter*/
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters){
+        converters.add(new YamlJackson2HttpMessageConverter());
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer){
@@ -38,9 +50,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .favorParameter(false)
                 .ignoreAcceptHeader(false)
                 .useRegisteredExtensionsOnly(false)
-                .defaultContentType(MediaType.APPLICATION_JSON)
+                .defaultContentType(MediaType.APPLICATION_JSON) // Valor de retorno default. Além desse são suportados os tipos abaixo:
                 .mediaType("json", MediaType.APPLICATION_JSON)
-                .mediaType("xml", MediaType.APPLICATION_XML);
+                .mediaType("xml", MediaType.APPLICATION_XML)
+                .mediaType("x-yaml", MEDIA_TYPE_YML);
+        ;
 
     }
 }
