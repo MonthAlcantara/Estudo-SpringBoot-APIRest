38a1ddafcf98d498039b5b06c1a1ae22096d85e0
monthalcantara <montival_junior@yahoo.com.br>
2020-12-16 09:33:52 -0300

    Reorganização do projeto para lista de repositorios

--- src/main/java/io/github/monthalcantara/restspringbootestudo/service/PersonService.java
+++
@@ -1,9 +0,0 @@
-package io.github.monthalcantara.restspringbootestudo.service;
-
-import io.github.monthalcantara.restspringbootestudo.model.Person;
-
-public interface PersonService {
-    public Person findById(String id);
-    public Person create(Person person);
-
-}
