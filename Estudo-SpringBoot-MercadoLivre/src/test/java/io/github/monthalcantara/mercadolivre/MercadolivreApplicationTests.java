3c8d1d333d3de093a1a5b7eca8b4eabe1de152db
monthalcantara <montival_junior@yahoo.com.br>
2020-12-19 15:40:34 -0300

    Adicionado novo projeto de estudo

---
+++ Estudo-SpringBoot-MercadoLivre/src/test/java/io/github/monthalcantara/mercadolivre/MercadolivreApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.mercadolivre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadolivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
