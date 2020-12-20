e4c699c18cd2af7ab3d759bf3701dad2fdfca832
monthalcantara <montival_junior@yahoo.com.br>
2020-12-20 12:54:16 -0300

    Adicionado consulta por like e retorno paginado

--- Estudo-SpringBoot-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/controller/UsuarioController.java
+++ Estudo-SpringBoot-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/controller/UsuarioController.java
@@ -2,8 +2,12 @@ package io.github.monthalcantara.mercadolivre.controller;
 
 import io.github.monthalcantara.mercadolivre.dto.request.NovoUsuarioRequest;
 import io.github.monthalcantara.mercadolivre.dto.response.UsuarioResponse;
+import io.github.monthalcantara.mercadolivre.exception.ApiErrorException;
 import io.github.monthalcantara.mercadolivre.model.Usuario;
 import io.github.monthalcantara.mercadolivre.repository.UsuarioRepository;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +17,10 @@ import org.springframework.web.util.UriComponentsBuilder;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/usuarios")
@@ -29,11 +35,15 @@ public class UsuarioController {
     @PostMapping
     @Transactional
     public ResponseEntity criaNovoUsuario(@Valid @RequestBody NovoUsuarioRequest usuarioRequest, UriComponentsBuilder builder) {
-        Usuario usuario = usuarioRequest.toModel();
+        Usuario usuario = converteUsuarioRequestEmUsuario(usuarioRequest);
         usuarioRepository.save(usuario);
 
         URI uri = builder.path("/usuarios/{id}").buildAndExpand(usuario.getId()).toUri();
         return ResponseEntity.created(uri).build();
+    }
+
+    private Usuario converteUsuarioRequestEmUsuario(@Valid NovoUsuarioRequest usuarioRequest) {
+        return usuarioRequest.toModel();
     }
 
     @GetMapping("/{id}")
@@ -45,5 +55,33 @@ public class UsuarioController {
         Usuario usuario = usuarioOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Usuario não encontrado"));
 
         return ResponseEntity.ok(new UsuarioResponse(usuario));
+    }
+
+    @GetMapping("/login/{login}")
+    @Transactional
+    public ResponseEntity buscaUsuarioPeloLogin(@PathVariable("login") String login, @PageableDefault(size = 5) Pageable pageable) {
+        List<Usuario> usuarios = usuarioRepository.buscaPorLoginQueContem(login, pageable).getContent();
+
+        verificaSeListaEstaVazia(usuarios);
+
+        return ResponseEntity.ok(converteListaUsuarioEmPageResponse(usuarios));
+    }
+
+    private PageImpl<UsuarioResponse> converteListaUsuarioEmPageResponse(List<Usuario> usuarios) {
+        List<UsuarioResponse> usuarioResponseList = converteListaUsuariosEmListaResponse(usuarios);
+        return new PageImpl<>(usuarioResponseList);
+    }
+
+    private void verificaSeListaEstaVazia(List<Usuario> usuarios) {
+        if (usuarios.isEmpty()) {
+            throw new ApiErrorException(HttpStatus.NOT_FOUND, "Usuario não encontrado");
+        }
+    }
+
+    private List<UsuarioResponse> converteListaUsuariosEmListaResponse(List<Usuario> usuarios) {
+        return usuarios
+                .stream()
+                .map(usuario -> new UsuarioResponse(usuario))
+                .collect(Collectors.toList());
     }
 }
--- Estudo-SpringBoot-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/repository/UsuarioRepository.java
+++ Estudo-SpringBoot-MercadoLivre/src/main/java/io/github/monthalcantara/mercadolivre/repository/UsuarioRepository.java
@@ -1,7 +1,11 @@
 package io.github.monthalcantara.mercadolivre.repository;
 
 import io.github.monthalcantara.mercadolivre.model.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +15,9 @@ import java.util.UUID;
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
 
     Optional<Usuario> findByLogin(String login);
+
+    @Query("Select u from Usuario u where u.login like %:login%")
+    Page<Usuario> buscaPorLoginQueContem(@Param("login") String login, Pageable pageable);
+
+    boolean existsUsuarioByLogin(String login);
 }
