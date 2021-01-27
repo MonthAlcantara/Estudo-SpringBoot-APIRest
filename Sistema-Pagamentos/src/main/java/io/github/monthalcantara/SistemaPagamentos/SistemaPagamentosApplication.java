4e1c2b2dbcac5b38b20488072cc3cc15e149be64
monthalcantara <montival_junior@yahoo.com.br>
2021-01-26 21:44:06 -0300

    Adicionado novo projeto- Serviço de pagamento

---
+++ Sistema-Pagamentos/src/main/java/io/github/monthalcantara/SistemaPagamentos/SistemaPagamentosApplication.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.SistemaPagamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaPagamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaPagamentosApplication.class, args);
+	}
+
+}
---
+++ Sistema-Pagamentos/src/test/java/io/github/monthalcantara/SistemaPagamentos/SistemaPagamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.monthalcantara.SistemaPagamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaPagamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
